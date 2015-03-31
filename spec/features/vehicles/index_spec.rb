f8ade4c0eeca5dcce59ff032b27577b46bb2a6fa
Rafael Flores <rafael.flores@ensobusiness.com>
2015-03-31 16:48:40 -0400

    Added test requirements and associated code for the vehicles view

--- spec/features/vehicles/index_spec.rb
+++ spec/features/vehicles/index_spec.rb
@@ -5,23 +5,43 @@ describe "Viewing Vehicles index" do
 let!(:customer) { Customer.create(first_name: "Rafael", last_name: "Flores", addr1: "Finca Elena", addr2: "32 calle Jobos", 
 							city: "Guaynabo", state: "PR", zip: "00971", h_phone: "7872874630", c_phone: "7875258367", 
 							email: "raflores1@me.com") }
-	before do
-
-		visit "/customers"
-			within "#customer_#{customer.id}" do
-				click_link "List Vehicles"
-			end
+		def visit_customer_vehicle(customer)
+				visit "/customers"
+				within "#customer_#{customer.id}" do
+					click_link "List Vehicles"
+				end
 		end
 
 		it "directs you to the vehicle index page" do
-
+			visit_customer_vehicle(customer)
 			within ("h1") do 
-				expect(page).to have_content("Vehicle List")
+				expect(page).to have_content("Rafael Flores's Vehicles")
 			end
 		end	
 	
 		it "displays no items when there is no vehicles created" do
-	
-				expect(vehicle.count).to eq (0)
+				visit_customer_vehicle(customer)
+				expect(page.all("tbody.customer tr").size).to eq(0)
 		end
+
+		it "displays the vehicle listing for a customer when there are vehicles created for that customer" do
+				customer.vehicles.create(vin: "78789909887900877VS", manufacturer: "Honda", year: "2014", model: "Accord", trim: "6V/Cruiser")
+				customer.vehicles.create(vin: "58605909654868486984", manufacturer: "Chevrolet", year: "2010", model: "Lumina", trim: "6v ls")
+				visit_customer_vehicle(customer)
+					expect(page).to have_content("Honda")
+					expect(page).to have_content("Chevrolet")
+
+		end
+
+		it "renders the show view when pressing the show button" do
+			customer.vehicles.create(vin: "78789909887900877VS", manufacturer: "Honda", year: "2014", model: "Accord", trim: "6V/Cruiser")
+			visit_customer_vehicle(customer)
+			expect(page).to have_content("Honda")
+		within ("tr#vehicle_#{customer.id}") do
+			click_link "Show"
+end
+			expect(page).to have_content("Vehicle Information")
+			expect(page).to have_content("Honda")
+end
+
 	end
